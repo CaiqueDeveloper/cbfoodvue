5dc8cec1dbe96035c8b5ed676aff1589c24999fa
Caique Bispo <caiquebispo0023@gmail.com>
2023-01-27 09:02:36 -0300

    Adicionando informação da empresa na URL

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/views/auth/LoginView.vue'
 import RequestFreeYourDemo from '@/views/auth/RequestFreeYourDemoView.vue'
-
+let appname = " - Cbfood Delivery";
 function lazyLoad(view){
   return() => import(`@/views/${view}.vue`)
 }
@@ -10,67 +10,67 @@ const routes = [{
     path: '/admin/dashboard',
     name: 'dashboard',
     component: lazyLoad('dashboard/Dashboard'),
-    meta: {layout: 'dashboard'}
+    meta: {layout: 'dashboard', title: "Dashboard " + appname}
   },{
     path: '/admin/company',
     name: 'company',
     component: lazyLoad('company/Company'),
-    meta: {layout: 'dashboard'}
+    meta: {layout: 'dashboard' , title: "Company " + appname}
   },{
     path: '/admin/permissions',
     name: 'permissions',
     component: lazyLoad('permission/Permission'),
-    meta: {layout: 'dashboard'}
+    meta: {layout: 'dashboard', title: "Permission " + appname}
   },{
     path: '/admin/users',
     name: 'users',
     component: lazyLoad('user/User'),
-    meta: {layout: 'dashboard'}
+    meta: {layout: 'dashboard', title: "User " + appname}
   },{
     path: '/admin/systemUsabilityControl',
     name: 'systemUsabilityControl',
     component: lazyLoad('systemUsabilityControl/SystemUsabilityControl'),
-    meta: {layout: 'dashboard'}
+    meta: {layout: 'dashboard', title: "System Usability Control " + appname}
   },{
     path: '/admin/import',
     name: 'import',
     component: lazyLoad('import/ImportReport'),
-    meta: {layout: 'dashboard'}
+    meta: {layout: 'dashboard', title: "Import Report " + appname}
   },{
     path: '/admin/deliveries',
     name: 'deliveries',
     component: lazyLoad('deliveries/Deliveries'),
-    meta: {layout: 'dashboard'}
+    meta: {layout: 'dashboard', title: "Deliveries " + appname}
   },{
     path: '/admin/orders',
     name: 'orders',
     component: lazyLoad('orders/Orders'),
-    meta: {layout: 'dashboard'}
+    meta: {layout: 'dashboard', title: "Orders " + appname}
   },{
     path: '/admin/category',
     name: 'category',
     component: lazyLoad('category/Category'),
-    meta: {layout: 'dashboard'}
+    meta: {layout: 'dashboard', title: "Category " + appname}
   },{
     path: '/admin/products',
     name: 'products',
     component: lazyLoad('product/Products'),
-    meta: {layout: 'dashboard'}
+    meta: {layout: 'dashboard', title: "Product " + appname}
   },{
     path: '/admin/additional',
     name: 'additional',
     component: lazyLoad('additional/Additional'),
-    meta: {layout: 'dashboard'}
+    meta: {layout: 'dashboard', title: "Additional " + appname}
   },{
     path: '/login',
     name: 'login',
     component: Login,
-    meta: {layout: 'auth'}
+    meta: {layout: 'auth' , title: "Login " + appname}
   },{
     path: '/requestFreeYourDemo',
     name: 'requestFreeYourDemo',
     component: RequestFreeYourDemo,
-    meta: {layout: 'auth'}
+    meta: {layout: 'auth', title: "Request Free Your Demo " + appname}
   }
 ]
 
@@ -85,7 +85,7 @@ router.beforeEach((to, from, next) => {
   const publicPages = ['/login', '/requestFreeYourDemo'];
   const authRequired = !publicPages.includes(to.path);
   const loggedIn = localStorage.getItem('access_token');
-
+  document.title = to.meta.title;
   if (authRequired && !loggedIn) {
     next('/login');
   } else {
